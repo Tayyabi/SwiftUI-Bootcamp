e8f5de3f3304aade382af46e4246ad04a679b862
Muhammad Tayyab <muhammadtayyab703@gmail.com>
2023-05-15 18:10:47 +0400

    Initial Commit

---
+++ Bootcamp/BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampApp.swift
+//  Bootcamp
+//
+//  Created by M Tayyab on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
